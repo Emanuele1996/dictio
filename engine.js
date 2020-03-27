480a32aa9366a70671caafc39ab13e7bd66123cf
Emanuele1996 <37412788+Emanuele1996@users.noreply.github.com>
2020-03-27 17:41:27 +0100

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,4 +1,4 @@
-$.getJSON( "ajax/https://rawcdn.githack.com/Emanuele1996/dictio/0f0ea02dad16a3d512f2427a272013fa7de45fec/citta.json", function( data ) {
+$.getJSON( "ajax/citta.json", function( data ) {
   var items = [];
   $.each( data, function( key, val ) {
     items.push( "<li id='" + key + "'>" + val + "</li>" );
